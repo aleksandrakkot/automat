9d20abc19b5bdc506234281f68a74a0066750990
Aleksandra Kot <aleksandrak.kot@gmail.com>
2021-05-31 12:14:55 +0200

    31.05

--- gui.py
+++ gui.py
@@ -1,17 +1,20 @@
 import tkinter as tk
+import tkinter.messagebox as msg
 from functools import partial
+from maszyna import Automat, nominaly, Towar
 
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.init_window()
+        self.title("Automat")
+        self.automat = Automat(towary=Towar.stworz_towary())
 
     def init_window(self):
         monety_frame = tk.Frame(master=self) #tworzymy sobie ramke przechowujaca przyciski a jego rodzicem (ramka nadrzedna) jest to okno czyli self
-        nominaly = ["5zł", "2zł", "1zł", "50gr", "20gr", "10gr", "5gr", "2gr", "1gr"]
         for nominal in nominaly:
-            b = tk.Button(master=monety_frame, text=nominal, width=10, height=5)
+            b = tk.Button(master=monety_frame, text=nominal, width=10, height=5, command=partial(self.przycisk_wplata_akcja, nominal))
             b.pack() #zapakuj sie do ramki
 
         #pakujemy ramke do okna
@@ -19,20 +22,17 @@ class Window(tk.Tk):
 
         prawy_frame = tk.Frame(master=self)
 
-        info_frame = tk.Frame(master=prawy_frame)
-        L1 = tk.Label(master=info_frame, text="Nr towaru")
-        L1.pack(side=tk.LEFT)
-
-        self.cyfry_var = tk.StringVar()
-        E1 = tk.Entry(master=info_frame, bd=5, textvariable=self.cyfry_var)
-        E1.pack(side=tk.RIGHT)
+        info_frame, self.cyfry_var = self.create_numeric_info(prawy_frame, "Nr towaru")
         info_frame.grid(column=0, row=0)
+
+        info_frame2, self.wplata = self.create_numeric_info(prawy_frame, "Wpłata")
+        info_frame2.grid(column=0, row=1)
 
         cyfry_frame = tk.Frame(master=prawy_frame)
         cyfry = [["1", "2", "3"],
                  ["4", "5", "6"],
                  ["7", "8", "9"],
-                 ["C", "0", "R"]]
+                 ["@", "0", "R"]]
 
         for i in range(len(cyfry)):
             for j in range(len(cyfry[i])):
@@ -43,17 +43,43 @@ class Window(tk.Tk):
                               command=partial(self.przycisk_cyfra_akcja, cyfry[i][j])) #https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
                 b.grid(row=i, column=j)
 
-        cyfry_frame.grid(column=0, row=1)
+        cyfry_frame.grid(column=0, row=2)
         prawy_frame.grid(column=1, row=0)
 
     def przycisk_cyfra_akcja(self, nr):
         if nr.isdigit():
             text = self.cyfry_var.get() + nr
             self.cyfry_var.set(text)
-        elif nr == "C":
+        elif nr == "R":
             self.cyfry_var.set("")
-        #TODO: dodac realizacje zamowienia (R)
+        elif nr == "@":
+            if self.cyfry_var.get() == "":
+                msg.showinfo("Błąd", "Nie podano nr towaru!")
+            else:
+                wiadomosc, ok = self.automat.zamowienie(int(self.cyfry_var.get()))
+                if ok:
+                    msg.showinfo("Wykonano!", wiadomosc)
+                    self.cyfry_var.set("")
+                    self.wplata.set("")
+                else:
+                    msg.showerror("Błąd", wiadomosc)
 
+
+
+    def przycisk_wplata_akcja(self, moneta_str):
+        moneta = nominaly[moneta_str]
+        self.automat.wrzuc(moneta)
+        self.wplata.set(f"{self.automat.suma_wplat()}zł")
+
+    def create_numeric_info(self, master, text):
+        info_frame = tk.Frame(master=master)
+        L1 = tk.Label(master=info_frame, text=text)
+        L1.pack(side=tk.LEFT)
+
+        var = tk.StringVar()
+        E1 = tk.Entry(master=info_frame, bd=5, textvariable=var, state=tk.DISABLED)
+        E1.pack(side=tk.RIGHT)
+        return info_frame, var
 
     def start(self):
         self.mainloop()
--- maszyna.py
+++ maszyna.py
@@ -1,5 +1,16 @@
 import random
 
+nominaly = {"5zł" : 5,
+             "2zł" : 2,
+             "1zł" : 1,
+             "50gr" : 0.5,
+             "20gr" : 0.2,
+             "10gr" : 0.1,
+             "5gr" : 0.05,
+             "2gr" : 0.02,
+             "1gr": 0.01
+             }
+
 
 class Towar:
     @staticmethod
@@ -26,15 +37,23 @@ class Towar:
     def ilosc(self):
         return self.__ilosc
 
+    @ilosc.setter
+    def ilosc(self, value):
+        self.__ilosc = value
 
 class Automat:
     def __init__(self, towary):
+        for towar in towary:
+            print(towar)
         self.__towary = towary
         self.__monety_wrzucone = []
         self.__monety_reszta = []
 
     def wrzuc(self, moneta):
         self.__monety_wrzucone.append(moneta)
+
+    def suma_wplat(self):
+        return round(sum(self.__monety_wrzucone), 2)
 
     def pobierz_towar(self, nr):
         try:
@@ -46,6 +65,7 @@ class Automat:
         self.__monety_wrzucone.clear()
 
     def podaj_reszte(self, ile_reszty):
+        ile_reszty = round(ile_reszty, 2)
         reszta_do_wydania = []
         monetki = self.__monety_reszta + self.__monety_wrzucone
         monetki.sort(reverse=True)
@@ -53,33 +73,36 @@ class Automat:
         while ile_reszty > 0 and index < len(monetki):
             if monetki[index] <= ile_reszty:
                 ile_reszty -= monetki[index]
+                ile_reszty = round(ile_reszty, 2)
                 reszta_do_wydania.append(monetki[index])
             index += 1
         if ile_reszty == 0:
             return reszta_do_wydania
 
     def zamowienie(self, nr):
+        sukces = False
         towar = self.pobierz_towar(nr)
 
         if not towar:
-            return "brak towaru o danym numerze"
+            return "brak towaru o danym numerze", sukces
 
         reszta = sum(self.__monety_wrzucone) - towar.cena
         if reszta < 0:
-            return "niewystarczajace srodki"
+            return "niewystarczajace srodki", sukces
 
         if towar.ilosc == 0:
-            return "brak towaru na stanie"
+            return "brak towaru na stanie", sukces
 
-        reszta_do_wydania = self.__monety_reszta(reszta)
-        if reszta_do_wydania != 0:
+        reszta_do_wydania = self.podaj_reszte(reszta)
+        if reszta_do_wydania is not None:
             towar.ilosc -= 1
             self.__monety_reszta.extend(self.__monety_wrzucone)
             self.__monety_wrzucone.clear()
             for moneta in reszta_do_wydania:
                 self.__monety_reszta.remove(moneta)
-            return f"Kupilas towar, wydano: {reszta_do_wydania}"
+            sukces = True
+            return f"Kupilas towar, wydano reszty: {sum(reszta_do_wydania)}zł", sukces
         else:
-            return "nie mozna wydac reszty"
+            return "nie mozna wydac reszty", sukces
 
 
